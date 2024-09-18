0f0c808557dd816773b8f2404c51ab52b9988f7d
halaabushehab <hala.abushiihab@gmail.com>
2024-09-18 13:39:08 +0300

    complet btn

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let tasks=[
 {
     "titel":"reading book",
     "date":"15/5/2025",
-    "Isdone":false,
+    "Isdone":true,
 },
 {
     "titel":"wlaking",
@@ -19,13 +19,14 @@ let tasks=[
 ]
 
 function fillTask(){
+
 $('.tasks').html("")
 let index=0
 for(task of tasks){
 
     let content=
     `  <!-- tasks -->
-        <div   class="tasks">
+        <div   class="tasks ${task.Isdone ? 'done':''}">
                 <!-- tasks inf -->
                 <div id="task__inf">
                 <h2>${task.titel}</h2>
@@ -37,7 +38,14 @@ for(task of tasks){
                      <!-- task action -->
                      <div class="task_action">
                       <button onclick=" deletTask(${index})" class="header_btn"  style="background-color: red"  ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: antiquewhite;transform: ;msFilter:;"><path d="M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z"></path><path d="M9 10h2v8H9zm4 0h2v8h-2z"></path></svg></button>
-                      <button  class="header_btn"  style="background-color: green"  ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: antiquewhite;transform: ;msFilter:;"><path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"></path></svg></button>
+ 
+                      ${task.Isdone ? `
+                      <button onclick=" completeTask(${index})"    class="header_btn"  style="background-color: green"  ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: antiquewhite;transform: ;msFilter:;"><path d="M9.172 16.242 12 13.414l2.828 2.828 1.414-1.414L13.414 12l2.828-2.828-1.414-1.414L12 10.586 9.172 7.758 7.758 9.172 10.586 12l-2.828 2.828z"></path><path d="M12 22c5.514 0 10-4.486 10-10S17.514 2 12 2 2 6.486 2 12s4.486 10 10 10zm0-18c4.411 0 8 3.589 8 8s-3.589 8-8 8-8-3.589-8-8 3.589-8 8-8z"></path></svg></button>
+
+
+                        `:`
+                      <button onclick=" completeTask(${index})"    class="header_btn"  style="background-color: green"  ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: antiquewhite;transform: ;msFilter:;"><path d="m10 15.586-3.293-3.293-1.414 1.414L10 18.414l9.707-9.707-1.414-1.414z"></path></svg></button>
+                      `}
                       <button  onclick=" editTask(${index})"  class="header_btn"   style="background-color:  #F3A2BE "  ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: antiquewhite;transform: ;msFilter:;"><path d="M19.045 7.401c.378-.378.586-.88.586-1.414s-.208-1.036-.586-1.414l-1.586-1.586c-.378-.378-.88-.586-1.414-.586s-1.036.208-1.413.585L4 13.585V18h4.413L19.045 7.401zm-3-3 1.587 1.585-1.59 1.584-1.586-1.585 1.589-1.584zM6 16v-1.585l7.04-7.018 1.586 1.586L7.587 16H6zm-2 4h16v2H4z"></path></svg> </button>
                       </div>
                       <!-- //task action //-->
@@ -87,3 +95,18 @@ function editTask(index){
        }
     
     
+function completeTask(index){
+    let task=tasks[index]
+    if(task.Isdone){
+        task.Isdone=false;
+    }
+    else{
+        task.Isdone=true;
+    }
+    //يختصر كل الجملة الشرطية الي فوقه بكفي  لحاله
+    // task.Isdone=!task.Isdone;
+    
+
+    fillTask();
+
+}
